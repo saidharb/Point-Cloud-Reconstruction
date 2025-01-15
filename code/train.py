a0dfb405bf4dd6eeee2d5756dc7f599b22a07366
Said Harb <said1harb@yahoo.de>
2025-01-15 11:24:08 +0100

    Implemented main method

--- code/train.py
+++ code/train.py
@@ -9,6 +9,7 @@ import torch
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+import json
 
 class PointCloudEmbeddingDataset(Dataset):
     
@@ -105,14 +106,37 @@ def parse_args():
         'Train PointNet++ to encode point clouds into the same latent space as DeepCAD '
         'does with CAD Sequences.'
     )
-    parser.add_argument('--data_root', type=str, required=True, help='Data directory')
+    # for the data_root argument: 
+    # I want the user to give the data directory from the root directory
+    # It would be kind of laborious to construct the relative file path to data from this script
+    parser.add_argument('--data_root', type=str, default='data', help='data directory relative to root directory')
+    parser.add_argument('--batch_size', type=int, default=24, help='batch size in training')
     return parser.parse_args()
 
 def main(args):
-    print(args.data_root)
+
+    # Find data directory
+    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    if os.getcwd() != root_dir:
+        DATA_DIR = os.path.join(root_dir, args.data_root)
+    else:
+        DATA_DIR = os.path.abspath(args.data_root)
+
+    # Print parameters
+    print("### Parameters ###\n", flush=True)
+    print(f"Data directory: {DATA_DIR}", flush=True)
+    print(f"Batch size: {args.batch_size}", flush=True)
+    print("", flush=True)
+
+    train_dataset = PointCloudEmbeddingDataset(DATA_DIR, 'test')
+    train_dataloader = DataLoader(train_dataset, batch_size = args.batch_size, shuffle = True)
 
 if __name__ == '__main__':
     args = parse_args()
     main(args)
 
     
+
+# For README:
+# Execute train.py either from root or from ./code
+# Give the data directory always relative to root (makes it easier)
