3cd7e2616f1a9e82ff77f400fad7bbc419dbb64d
Said Harb <said1harb@yahoo.de>
2025-02-11 10:31:34 +0100

    Added test to check matching latent representations

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -2,18 +2,29 @@ import os
 import random
 import pytest
 
-from code.dataset import PointCloudEmbeddingDataset
+import torch
 
+from code.dataset import PointCloudEmbeddingDataset, PointCloudEmbeddingSequenceDataset
+from models.DeepCAD.config.configAE import ConfigAE
+from models.DeepCAD.trainer.trainerAE import TrainerAE
 
-# Check if the Point Cloud and latent representation provided by PointCloudEmbeddingDataset match
 DATA_DIR = "data"
 
-# Check if PC and CAD-sequence paths match in PN++ training
 @pytest.mark.parametrize("split", ["train", "validation", "test"])
-def test_pc_lat_rep_match(split):
+def test_PointCloudEmbeddingDataset_empty(split):
+    """Check if the datasets are not empty."""
     dataset = PointCloudEmbeddingDataset(DATA_DIR, split)
-
     assert len(dataset) > 0, f"Dataset {split} is empty."
+@pytest.mark.parametrize("split", ["train", "validation", "test"])
+def test_PointCloudEmbeddingSequenceDataset_empty(split):
+    """Check if the datasets are not empty."""
+    dataset = PointCloudEmbeddingSequenceDataset(DATA_DIR, split)
+    assert len(dataset) > 0, f"Dataset {split} is empty."
+
+@pytest.mark.parametrize("split", ["train", "validation", "test"])
+def test_pc_cad_seq_match(split):
+    """Check whether the point cloud path and CAD-sequence path match"""
+    dataset = PointCloudEmbeddingDataset(DATA_DIR, split)
 
     idx = random.randint(0, len(dataset) - 1)
     pc_path = dataset.get_pc_path(idx)
@@ -23,6 +34,47 @@ def test_pc_lat_rep_match(split):
         os.path.splitext(os.path.basename(pc_path))[0] ==
         os.path.splitext(os.path.basename(cad_path))[0]
     ), "Point Cloud and CAD-Sequence don't align in PointCloudEmbeddingDataset {split} set."
+@pytest.mark.parametrize("split", ["train", "validation", "test"])
+def test_pc_cad_seq_match(split):
+    """Check whether the point cloud path and CAD-sequence path match"""
+    dataset = PointCloudEmbeddingSequenceDataset(DATA_DIR, split)
+
+    idx = random.randint(0, len(dataset) - 1)
+    pc_path = dataset.get_pc_path(idx)
+    cad_path = dataset.get_cad_seq_path(idx)
+
+    assert (
+        os.path.splitext(os.path.basename(pc_path))[0] ==
+        os.path.splitext(os.path.basename(cad_path))[0]
+    ), "Point Cloud and CAD-Sequence don't align in PointCloudEmbeddingSequenceDataset {split} set."
+
+
+@pytest.mark.parametrize("split", ["train", "validation", "test"])
+def test_cad_vec_lat_rep_match(split):
+    """ Test whether the target latent representations of the point cloud sampled 
+        from its CAD-sequence in PN++ training matches the encoded CAD-sequences 
+        by DeepCAD.
+        ATTENTION: If the pretrained DeepCAD model specified in ConfigAE changes,
+        the latent representations have to be encoded again by the new DeepCAD model.
+        """ 
+    
+    dataset = PointCloudEmbeddingSequenceDataset(DATA_DIR, split)
+    idx = random.randint(0, len(dataset) - 1)
+    _, lat_rep, cad_vec = dataset[idx]
+    command = cad_vec[:, 0]
+    args = cad_vec[:, 1:]
+    cad_vec = {"command": command, "args": args}
+
+    cfg = ConfigAE('test')
+    tr_agent = TrainerAE(cfg)
+    tr_agent.load_ckpt(cfg.ckpt)
+    tr_agent.net.eval()
+
+    with torch.no_grad():
+        output = tr_agent.encode(cad_vec)
+        output = output.squeeze()
+        assert(torch.allclose(lat_rep, output, atol=1e-6)), "The encoded CAD-\
+            sequence does not match the latent representation on disk."
 
 
 # schauen ob pretrained DeepCAD model die selbe lat rep produziert
