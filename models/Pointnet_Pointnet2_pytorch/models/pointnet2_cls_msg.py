f0b1f8f10c37da8eabaa2623b2e1c1e13ae6a5fc
Said Harb <said1harb@yahoo.de>
2025-03-18 15:18:39 +0100

    Created new model architecture (same as authors)

--- models/Pointnet_Pointnet2_pytorch/models/pointnet2_cls_msg.py
+++ models/Pointnet_Pointnet2_pytorch/models/pointnet2_cls_msg.py
@@ -36,7 +36,50 @@ class get_model(nn.Module):
 
 
         return x,l3_points
+    
+class get_model_new(nn.Module):
+    """
+        CHANGED
+        - Adapted radius, n_sample and mlp's for sa1, sa2, sa3
+        - Optional Bias in linear layers, here: NO BIAS
+        ADDED
+        - Added sa4
+        - final tanh activation
+        REMOVED
+        - BatchNorm1d from head
+        - Dropout from head
+    """
+    def __init__(self,num_class,normal_channel=True):
+        super(get_model, self).__init__()
+        in_channel = 3 if normal_channel else 0
+        self.normal_channel = normal_channel
+        self.sa1 = PointNetSetAbstractionMsg(512, [0.1], [64], in_channel,[[32, 32, 64]], bias=False)
+        self.sa2 = PointNetSetAbstractionMsg(256, [0.2], [64], 64,[[64, 64, 128]], bias=False)
+        self.sa3 = PointNetSetAbstractionMsg(128, [0.4], [64], 128,[[128, 128, 256]], bias=False)
+        self.sa4 = PointNetSetAbstraction(None, None, None, 256+3, [256, 512, 1024], True, bias=False)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, num_class)
+        self.tanh = nn.Tanh()
 
+    def forward(self, xyz):
+        B, _, _ = xyz.shape
+        if self.normal_channel:
+            norm = xyz[:, 3:, :]
+            xyz = xyz[:, :3, :]
+        else:
+            norm = None
+        l1_xyz, l1_points = self.sa1(xyz, norm)
+        l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
+        l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
+        l4_xyz, l4_points = self.sa4(l3_xyz, l3_points)
+
+        x = l4_points.view(B, 1024)
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
+        x = self.tanh(self.fc3(x))
+
+        return x,l4_points
 
 class get_loss_nll(nn.Module):
     def __init__(self):
